0faafa6fe757a5c7ce6c77a24183443a96907bbe
Svein Olav Styve <97svesty@dhcp-10-22-46-108.wlan.ntnu.no>
2017-09-14 12:47:06 +0200

    HTML og CSS funker

--- website/testApp/views.py
+++ website/testApp/views.py
@@ -2,7 +2,26 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import Context, loader
+from django.shortcuts import render_to_response
+
 
 # Create your views here.
-def index(request):     #This is the index page (main page) for testApp
+def index2(request):     #This is the index page (main page) for testApp
     return HttpResponse("Hei. Dette er en testside")
+
+def index3(request):
+    template = loader.get_template("testApp/index.html")
+    return HttpResponse(template.render(request))
+
+def index4(request):
+    return render_to_response('testApp/index.html')
+
+def index5(request):
+    # View code here...
+    return render(request, 'testApp/index.html', {
+        'foo': 'bar',
+    }, content_type='application/xhtml+xml')
+
+def index(request):
+    return render(request, 'testApp/index.html')
