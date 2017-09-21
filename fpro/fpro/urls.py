aded52bc824e401874a991b1ec467f599d2906b5
Svein Olav Styve <97svesty@dhcp-10-22-44-38.wlan.ntnu.no>
2017-09-21 13:26:37 +0200

    creat booking koblet mot db

--- fpro/fpro/urls.py
+++ fpro/fpro/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from . import views
+
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
-    url(r'^create_booking$', TemplateView.as_view(template_name='create_booking.html'), name='create_booking'),
+	url(r'^create_booking$', views.create_booking, name="create_booking"),
+    #url(r'^create_booking$', TemplateView.as_view(template_name='create_booking.html'), name='create_booking'),
     url(r'^admin/', admin.site.urls),
 ]
--- fpro/fpro/views.py
+++ fpro/fpro/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+from .models import Booking
+from .models import Scene
+from .models import Band
+
 
 # Create your views here.
+def create_booking(request):
+	bookings = Booking.objects.all()
+	template = loader.get_template('create_booking.html')
+	context = {
+        'bookings': bookings,
+    }
+	return HttpResponse(template.render(context, request))
