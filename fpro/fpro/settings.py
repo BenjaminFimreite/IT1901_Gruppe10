2b3a427a66c9ee7a1e789b6b41696fda10fac8bf
Svein Olav Styve <97svesty@dhcp-10-22-44-38.wlan.ntnu.no>
2017-09-21 13:43:32 +0200

    added assets folder, settungs updated accordingly, creat booking image

--- fpro/fpro/settings.py
+++ fpro/fpro/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+    '/var/www/assets/',
+]
+
 LOGIN_REDIRECT_URL = 'home'
 
 #ROLE PERMISSIONS
