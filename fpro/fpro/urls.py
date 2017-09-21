a06b37d672ee7ac184687daf694c43cbd25390f7
Benjamin Fimreite <benjamfi@stud.ntnu.no>
2017-09-21 13:04:47 +0200

    la til shifts

--- fpro/fpro/urls.py
+++ fpro/fpro/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
-    url(r'^create_booking$', TemplateView.as_view(template_name='create_booking.html'), name='create_booking'),
     url(r'^admin/', admin.site.urls),
+    url(r'^create_booking$', TemplateView.as_view(template_name='create_booking.html'), name='create_booking'),
+    url(r'^shifts$', TemplateView.as_view(template_name='shifts.html'), name='shifts'),
 ]
