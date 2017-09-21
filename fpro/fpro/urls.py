ad98b8d08b1ed9ffe1d401844704156776e2928f
Martin Bondkall Gjerde <martin.b.gjerde@gmail.com>
2017-09-21 12:18:50 +0200

    lagt til creat_booking.html

--- fpro/fpro/urls.py
+++ fpro/fpro/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+    url(r'^creat_booking$', TemplateView.as_view(template_name='create_booking.html'), name='create_booking'),
 ]
