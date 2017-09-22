b888d4eea367aac1774c7f9ad6e05bdc9bd144a6
ingridlarssen <ingrid_al_96@hotmail.com>
2017-09-22 23:15:11 +0200

    Satte opp sending med mail

--- fpro/fpro/settings.py
+++ fpro/fpro/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+#mailgreier
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'fpro.booking@gmail.com'
+EMAIL_HOST_PASSWORD = 'gruppe10'
+EMAIL_PORT = 587
+
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- fpro/fpro/urls.py
+++ fpro/fpro/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
 	url(r'^create_booking$', views.create_booking, name="create_booking"),
-    #url(r'^create_booking$', TemplateView.as_view(template_name='create_booking.html'), name='create_booking'),
+    url(r'^create_booking$', TemplateView.as_view(template_name='create_booking.html'), name='create_booking'),
     url(r'^admin/', admin.site.urls),
     url(r'^shifts$', views.shifts, name='shifts'),
 	url(r'^view_bookings$', views.view_bookings, name="view_bookings"),
 	url(r'^view_booking/(?P<booking_id>[0-9]+)/$', views.view_booking, name="view_booking"),
+    url(r'^send_email$', views.send_email, name="sendemail"),
+    url(r'^sentbooking$', TemplateView.as_view(template_name='sentbooking.html'), name="sentbooking")
+
 ]
