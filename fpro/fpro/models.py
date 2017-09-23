3fde6c19267a580db8a837510788d627beed3f98
svein007 <svein_001@hotmail.com>
2017-09-23 17:04:48 +0200

    endret booking model, technicians kan nå velges

--- fpro/fpro/models.py
+++ fpro/fpro/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.        
+# Create your models here.
 
 class Scene(models.Model):
         sceneName = models.CharField(max_length=100)
@@ -12,22 +12,19 @@ class Scene(models.Model):
 
 class Band(models.Model):
         bandName = models.CharField(max_length=100)
-        
+
         def __str__(self):
                 return self.bandName
-        
+
 class Booking(models.Model):
 	band = models.ForeignKey(Band, on_delete=models.CASCADE)
 	date = models.DateField('date playing')
 	approvedBookingBoss = models.BooleanField(default=False)
 	approvedManager = models.BooleanField(default=False)
-	technicalRequirements = models.CharField(max_length=100)
-	scene = models.ForeignKey(Scene, on_delete=models.CASCADE)
+	technicalRequirements = models.CharField(max_length=100, blank=True, default="")
+	scene = models.ForeignKey(Scene, on_delete=models.CASCADE, blank=True)
 	pris = models.DecimalField(max_digits=30, decimal_places=2)
-	technicians = models.CharField(max_length=200, default="hei")
+	technicians = models.ManyToManyField(User, blank=True)	# filtrer på technicians rolle/rettigheter
 
 	def __str__(self):
                 return self.band.bandName + " playing at " + str(self.date) + " at scene " + self.scene.sceneName
-
-	
-
