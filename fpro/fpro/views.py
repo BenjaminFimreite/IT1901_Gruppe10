2cb9d651fe9a73ec6c479e7a3a2fc276b9752956
svein007 <svein_001@hotmail.com>
2017-10-04 15:37:01 +0200

    fjerna genre på nytt

--- fpro/fpro/views.py
+++ fpro/fpro/views.py
@@ -190,15 +190,10 @@ def band(request, band_id):
 	}
 	return HttpResponse(template.render(context, request))
 
-
-
-
-
-
 def overview(request):
 	bookings = Booking.objects.all()
 	scenes = Scene.objects.all()
-	genres = Genre.objects.all()
+	## genres = Genre.objects.all()
 	coming_bookings = {}
 	past_bookings = []
 	for scene in scenes:
@@ -218,7 +213,7 @@ def overview(request):
 		'scenes' : scenes,
 	  	'bookings': bookings,
 		'bookings_array': coming_bookings,
-		'genres' : genres,
+		## 'genres' : genres,
 	}
 
 	return HttpResponse(template.render(context, request))
